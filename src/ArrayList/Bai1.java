e9b231a81707be7b384c09649c730a320acbbaeb
thaidat01222 <thaidat01222@gmail.com>
2020-04-15 20:03:28 +0700

    Initial commit

---
+++ src/ArrayList/Bai1.java
@@ -0,0 +1,31 @@
+package ArrayList;
+import java.util.ArrayList;
+import java.util.List;
+public class Bai1 {
+    public ArrayList<Student> students ;
+    public Bai1(){
+        this.students = new ArrayList<Student>();
+    }
+    public void addStudent(Student st) {
+        students.add(st);
+    }
+    public void deleteDocument(int id) {
+        students.remove(id);
+    }
+    public void updateStudent(int id, String nameChange){
+
+        for(Student st : students){
+           if(st.equals(id)){
+               st.setName(nameChange);
+           }
+       }
+    }
+    public List<Student> searchStudentByID(String id) {
+        return this.students.stream().filter(student -> student.getId()
+                .equals(id)).findFirst().orElse(null);
+    }
+    public void showStudent(){
+        students.forEach(student -> System.out.println(student.toString()));
+    }
+
+}
