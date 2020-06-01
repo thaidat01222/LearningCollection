ee81c4b5e9b0072e65bda45cf89e968d232f9e33
thaidat01222 <thaidat01222@gmail.com>
2020-06-01 23:19:06 +0700

    bai18(Update) + Bai28

--- src/LearnBasic/Bai18.java
+++ src/LearnBasic/Bai18.java
@@ -3,17 +3,23 @@ package LearnBasic;
 import java.util.Scanner;
 
 public class Bai18 {
-    public int Count_the_words(String string) {
+
+    public String Standardize_The_String(String string){
         String newString = string.trim();
+        int n = 0;
+        while (n<newString.length()){
+            newString = newString.replace("  ", " ");
+            n++;
+        }
+        return newString;
+    }
+    public int Count_the_words(String string) {
         int count = 0;
+        Bai18 bai18 = new Bai18() ;
+        String newString = bai18.Standardize_The_String(string);
         if (newString.isEmpty()) {
             return 0;
         } else {
-            int n = 0;
-            while (n < newString.length()) {
-                newString = newString.replace("  ", " ");
-                n++;
-            }
             for (int i = 1; i < newString.length(); i++) {
                 if((newString.charAt(i) == ' ')&&(newString.charAt(i+1) != ' ')) {
                     count++;
@@ -28,6 +34,8 @@ public class Bai18 {
         System.out.println("Nhap chuoi ma ban mong muon");
         String string = scanner.nextLine();
         Bai18 bai18 = new Bai18();
+        String newString = bai18.Standardize_The_String(string);
+        System.out.println("Chuoi sau khi duoc chuan hoa suong suong : " +newString);
         int count = bai18.Count_the_words(string);
         System.out.println("so chu cua chuoi la : " + count);
     }
